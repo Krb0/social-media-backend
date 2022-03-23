2466522d530613de990a92f23ff01446c9563214
Krb0 <maurodavidcaceres@gmail.com>
2022-03-23 09:29:40 -0300

    connection to mongo

--- index.ts
+++ index.ts
@@ -2,3 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts";
+
+dotenv.config();
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://${process.env.CONNECTION_USER}:${process.env.CONNECTION_PASS}@cluster0.bmscu.mongodb.net/test`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err));
